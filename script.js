983e663211ef12723a08f9eee567bcc9d0be7ea9
Lucas Bresolin <lucas.bresolin2248@gmail.com>
2023-09-29 09:54:14 -0300

    update

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%*|(){}[]/+-_";
+// let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%*|(){}[]/+-_";
+let charset = ""; 
+let charsetNumbers = "0123456789";
+let charsetLetters = "abcdefghijklmnopqrstuvwxyz";
+let charsetLettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let charsetSymbols = "!@#$%*|(){}[]/+-_";
 let novaSenha = "";
+
 
 sizePassword.innerHTML = sliderElement.value;
 
@@ -15,11 +21,23 @@ slider.oninput = function() {
     sizePassword.innerHTML = this.value;
 }
 
+if(document.querySelector("#numbers").checked) {
+    charset += charsetNumbers;
+}
+
+if(document.querySelector("#letters").checked) {
+    charset += charsetLetters;
+}
+
+if(document.querySelector("#symbols").checked) {
+    charset += charsetSymbols;
+}
+
 function generatePassword() {
     let pass = "";
 
     for(let i = 0, n = charset.length; i < sliderElement.value; ++i) {
-        pass += charset.charAt(Math.floor(Math.random() * n)); 
+        pass += charset.charAt(Math.floor(Math.random() * n));
     } 
 
     containerPassword.classList.remove("hide");
