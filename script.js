21f78361198925007acb516c60cf40f7721b9a4e
Lucas Bresolin <lucas.bresolin2248@gmail.com>
2023-09-29 10:14:14 -0300

    atualização de seleção

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let password = document.querySelector("#password");
 let containerPassword = document.querySelector("#container-password");
 
 // let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%*|(){}[]/+-_";
-let charset = ""; 
+let charset = "";
 let charsetNumbers = "0123456789";
 let charsetLetters = "abcdefghijklmnopqrstuvwxyz";
 let charsetLettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -17,28 +17,32 @@ let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oninput = function() {
+slider.oninput = function () {
     sizePassword.innerHTML = this.value;
-}
-
-if(document.querySelector("#numbers").checked) {
-    charset += charsetNumbers;
-}
-
-if(document.querySelector("#letters").checked) {
-    charset += charsetLetters;
-}
-
-if(document.querySelector("#symbols").checked) {
-    charset += charsetSymbols;
 }
 
 function generatePassword() {
     let pass = "";
-
-    for(let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+    let charset = "";
+    
+    if (document.querySelector("#numbers").checked) {
+        charset += charsetNumbers;
+    }
+    
+    if (document.querySelector("#lowercase").checked) {
+        charset += charsetLetters;
+    }
+    
+    if (document.querySelector("#uppercase").checked) {
+        charset += charsetLettersUpper;
+    }
+    
+    if (document.querySelector("#symbols").checked) {
+        charset += charsetSymbols;
+    }
+    for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
         pass += charset.charAt(Math.floor(Math.random() * n));
-    } 
+    }
 
     containerPassword.classList.remove("hide");
     password.innerHTML = pass;
