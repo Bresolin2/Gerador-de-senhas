f1ceecfbef30eff8bf4a2ea25c8fe6af4a1021e1
Lucas Bresolin <99617244+Bresolin2@users.noreply.github.com>
2023-09-28 21:56:50 -0300

    opa

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let buttonElement = document.querySelector("#button");
 let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
 
-let containerPassoword = document.querySelector("#container-password");
+let containerPassword = document.querySelector("#container-password");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%*|(){}[]/?;:.,<>~`^'+-_";
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%*|(){}[]/+-_";
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
@@ -19,9 +19,10 @@ function generatePassword() {
     let pass = "";
 
     for(let i = 0, n = charset.length; i < sliderElement.value; ++i) {
-        pass += charset.charAt(Math.floor(Math.random() * n));
+        pass += charset.charAt(Math.floor(Math.random() * n)); 
     } 
 
     containerPassword.classList.remove("hide");
     password.innerHTML = pass;
-};
+
+}
